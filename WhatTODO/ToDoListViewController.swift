2e791b788f39598e3f974b1728fe37af3b162ee3
cerenercan <36184074+cerenercan@users.noreply.github.com>
2020-06-22 19:31:27 +0300

    Added datasource methods

--- WhatTODO/ToDoListViewController.swift
+++ WhatTODO/ToDoListViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
+    let itemArray = ["item1", "item2", "item3"]
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    //MARK: - DataSource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
     }
 }
 
